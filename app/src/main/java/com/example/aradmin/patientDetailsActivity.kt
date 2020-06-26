d9eb729533548617407583a3d3aa36af132eb0e7
Shiina631 <42161993+Shiina631@users.noreply.github.com>
2020-06-26 17:58:59 +0800

    Initial commit

---
+++ app/src/main/java/com/example/aradmin/patientDetailsActivity.kt
@@ -0,0 +1,11 @@
+package com.example.aradmin
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class patientDetailsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_patient_details)
+    }
+}
---
+++ app/src/main/java/com/example/aradmin/treatmentActivity.kt
@@ -0,0 +1,11 @@
+package com.example.aradmin
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class treatmentActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_treatment)
+    }
+}
